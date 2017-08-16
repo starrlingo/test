244612337054325c149e81b38a4e398dd3eaf103
jasonxlin <jasonxlin@qnap.com>
2017-08-15 22:06:42 -0400

    Update monitor script format

--- shell/resourceMonitor.sh
+++ shell/resourceMonitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "timestamp, cpu(%), memory(%)"
 while true
 do
-    echo $(date +%FT%H:%M:%S), $(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'), $(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }') >> resource.log
-    sleep 10
+    echo $(date +%FT%H:%M:%S), $(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'), $(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2 }')
+    sleep 60
 done
